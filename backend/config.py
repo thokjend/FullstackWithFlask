769acde4a5c29b63acd3cb7415b1810489ef026c
thokjend <thokjend@online.no>
2024-11-07 13:33:20 +0100

    Initial commit

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLAlchemy_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import ContactList from "./components/ContactList";
+import "./App.css";
+import ContactForm from "./components/ContactForm";
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentContact, setCurrentContact] = useState({});
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://localhost:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    //console.log(data.contacts);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCurrentContact({});
+  };
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true);
+  };
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return;
+    setCurrentContact(contact);
+    setIsModalOpen(true);
+  };
+
+  const onUpdate = () => {
+    closeModal();
+    fetchContacts();
+  };
+
+  return (
+    <>
+      <ContactList
+        contacts={contacts}
+        updateContact={openEditModal}
+        updateCallback={onUpdate}
+      />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <ContactForm
+              existingContact={currentContact}
+              updateCallback={onUpdate}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
